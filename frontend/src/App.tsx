7ab2a7055cb6838d2e9447cf9d6747564abb5c3c
Brad Prolo <bprosurf@yahoo.com>
2023-03-28 18:08:58 -0600

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+//import ReactDOM from 'react-dom';
+import data from "./teams.json";
+import './index.css';
+
+const teamList = data.teams;
+
+function Welcome(){
+  return(
+   <center><h1>College Basketball Teams!!!</h1></center> 
+  );
+}
+// Create the teams class
+class Teams extends React.Component<{school: string; name: string; state: string; city: string;}>{
+  render()
+  {
+    const oneTeam = this.props;
+
+    return(
+      <div className="team-container">
+        {/* These are the the values that are passed from the JSON file */}
+        <h1>{oneTeam.school}</h1>   
+        <h3>Lets Go {oneTeam.name}!!</h3>
+        <h3>{oneTeam.city}, {oneTeam.state}</h3>
+      </div>
+    );
+  }
+}
+
+
+function TeamInfo(){
+  return(
+    <div className="team-info">
+      {teamList.map(oneTeam => <Teams {...oneTeam}/>)}
+    </div>
+  )
+}
+
+// this is that app that runs the classes
+function App() { 
+  return (
+    <div>
+      <Welcome/>
+      <div className="App">
+        <div className='col-9'>
+          <TeamInfo />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
